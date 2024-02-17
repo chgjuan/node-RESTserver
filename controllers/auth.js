ca071e0e84af452a62f08aaa4df5355dc12fad2e
Juan Pablo <juanpablo.chg12@gmail.com>
2024-02-17 08:24:39 -0500

    Google Sign-in and Sign-out

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ import { response, request } from "express"
 import { Usuario } from '../models/usuario.js'
 import bcryptjs from "bcryptjs"
 import { generarJWT } from "../helpers/generarJWT.js"
+import { googleVerify } from "../helpers/google-verify.js"
 
 
 const login = async (req, res = response) =>{
@@ -52,4 +53,52 @@ const login = async (req, res = response) =>{
     }
 }
 
-export { login }
+const googleSignIn = async(req, res) => {
+
+    const {id_token} = req.body
+
+    try {
+        const {correo, nombre, img}= await googleVerify( id_token )
+        // console.log(googleUser)
+
+        let usuario = await Usuario.findOne({correo})
+        if(!usuario){
+            //tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                rol: "USER_ROLE",
+                google: true,
+            }
+
+            usuario = new Usuario(data)
+            await usuario.save()
+        }
+
+        //Si el usuario en DB está negado - Estado
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        //Generar JWT
+        const token = await generarJWT(usuario.id)
+
+
+        res.json({
+            usuario,
+            token
+        })
+    }catch (err){
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+}
+
+export { login, googleSignIn}
